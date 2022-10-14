920de923a143b64206f8e8ec6d883fafad1ce2c3
Bruno Gonçalves Ferreira <brunogoncalvesferreira@outlook.com>
2022-10-14 08:15:21 -0300

    Commit

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,9 @@ import { Logo } from "./Logo";
 import "./styles/global.css";
 
 export function App() {
+
+  
+
   return (
     <div className="w-screen h-screen bg-gray-900 flex flex-col items-center justify-center text-gray-100">
         <header className="flex flex-col items-center">
